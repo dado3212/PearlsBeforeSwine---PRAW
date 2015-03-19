9a032f34ac24e3b2a42ac095b18d6b55eaca754d
dado3212 <alexcbeals@gmail.com>
2015-03-19 10:32:09 -0400

    Added in message if not posted by 5PM EST

--- pearls.py
+++ pearls.py
@@ -38,10 +38,24 @@ print '#####################################################################'
 r.send_message('dado3212','Booting','Pearls_Bot initializing.')
 ## LOOP CODE ##
 
+sent_message = ''
+
 running = True
 while running:
-	curr_date = "{dt.month}/{dt.day}/{year}".format(dt = datetime.datetime.now(pytz.timezone('US/Eastern')), year = strftime('%y'))
-	start = 'Pearls ' + curr_date + ': '
+	dt = datetime.datetime.now(pytz.timezone('US/Eastern'))	#creates time object
+	curr_date = "{dt.month}/{dt.day}/{year}".format(dt = dt, year = strftime('%y')) #gets the current date in the format mm/dd/yy, with no leading 0's
+	start = 'Pearls ' + curr_date + ': ' #gets the start of today's strip post (to see if it's been posted)
+
+	curr_hour = datetime.datetime.now(pytz.timezone('US/Eastern')).hour #gets the current 24 hour 
+
+	if (curr_hour == 17 and sent_message != curr_date): #if it's five pm
+		matches = 0
+		todays = sub.search('title:"' + start + '"') #and it hasn't been posted
+		for match in todays:
+			matches+=1
+		if (matches == 0): #send a message
+			r.send_message('dado3212','Pearls Post - ' + curr_date,'The current comic has not yet been posted.  \nThe link is [**here**](http://www.gocomics.com/pearlsbeforeswine/' + strftime('%Y/%m/%d') + ').  \nThe posting link is located [**here**](http://www.reddit.com/r/pearlsbeforeswine/submit?title=Pearls%20' + str(dt.month) + '/' + str(dt.day) + '/' + strftime('%y') + '%3A&url=http://www.gocomics.com/pearlsbeforeswine/' + strftime('%Y/%m/%d') + ').')
+			sent_message = curr_date
 
 	try:
 		links = sub.get_new(limit=3) #get 3 newest posts
@@ -67,4 +81,4 @@ while running:
 		print '----'
 	except Exception as e:
 		print 'Error: ' + str(e)
-	sleep(120) #wait for 2 minutes (minus 8 seconds)
+	sleep(120) #wait for 2 minutes
